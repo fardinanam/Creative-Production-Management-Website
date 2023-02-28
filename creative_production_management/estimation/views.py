128ea6ca2ced9e14b92df45fcf4265eac0ccd28c
Fardin Anam Aungon <52667920+fardinanam@users.noreply.github.com>
2023-03-01 02:30:03 +0600

    prev estimation and create est separated

--- creative_production_management/estimation/views.py
+++ creative_production_management/estimation/views.py
@@ -12,36 +12,55 @@ class CSOLoginView(CreateView):
   def post(self, request):
     username = request.POST['username']
     password = request.POST['password']
-
-    
-
     cso = None
 
     try:
       cso = CSO.objects.get(username=username, password=password)  
     except:
+      print("error logging in")
       return render(request, 'estimation/login.html')
 
     # url = reverse()
-    return redirect('create-estimation/' + str(cso.username) + '/')
+    return redirect('prev-estimations/' + str(cso.username) + '/')
+
+def prev_estimations(request, username:str):
+  if request.method == 'GET':
+    return render(request, 'estimation/prev_estimations.html', {'username': username})
+      
 
 def create_estimation(request, username:str):
 
+
   if request.method == 'POST':
+    name = request.POST['name']
+    estimated_duration_months = request.POST['estimated-duration-months']
+    description = request.POST['description']
+
     estimated_time = request.POST['estimated-time']
     estimated_cost = request.POST['estimated-cost']
-    description = request.POST['description']
 
     cso = CSO.objects.get(username=username)
     designer = Designer.objects.get(username="anup")
     tag = Tag.objects.get(name="video editor")
     vendor = Vendor.objects.get(name="Ananto")
-    designer_set = set()
-    designer_set.add(designer)
-    tag_set = set()
-    tag_set.add(tag)
-    estimation = Estimation(description=description, cso=cso, estimated_duration_months=estimated_time, estimated_cost=estimated_cost, designers=designer_set, tags=tag_set, vendors=vendor)
+    
+    # designer_set = set()
+    # designer_set.add(designer)
+    # tag_set = set()
+
+    # try:
+    estimation = Estimation.objects.create(description=description, cso=cso, estimated_duration_months=estimated_time, estimated_cost=estimated_cost)
+
+    # tag_set.add(tag)
+    # estimation = Estimation(description=description, cso=cso, estimated_duration_months=estimated_time, estimated_cost=estimated_cost, designers=designer_set, tags=tag_set, vendors=vendor)
     estimation.save()
-  return render(request, 'estimation/estimation.html', {'username': username})
+
+    estimation.designers.add(designer)
+    estimation.tags.add(tag)
+    estimation.vendors.add(vendor)
+    # except:
+    #   print("error")
+    
+  return render(request, 'estimation/create_estimation.html', {'username': username})
 
 
