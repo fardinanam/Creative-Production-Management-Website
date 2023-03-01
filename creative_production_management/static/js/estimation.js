ad582b77bc00ac61b5f94807c113e6fbb010135c
Fardin Anam Aungon <52667920+fardinanam@users.noreply.github.com>
2023-03-01 11:09:50 +0600

    changed

--- creative_production_management/static/js/estimation.js
+++ creative_production_management/static/js/estimation.js
@@ -4,6 +4,9 @@ const estimationNameInput = document.getElementById("estimation-name");
 const estimationDurationFields = document.getElementsByClassName("estimated-duration");
 const estimationDurationInput = document.getElementById("estimated-duration");
 
+// const estimationCostFields = document.getElementsByClassName("estimated-cost");
+// const estimationCostInput = document.getElementById("estimated-cost");
+
 const addDesignerContainer = document.querySelector("[add-designer-container]");
 const addDesignerTemplate = document.querySelector("[add-designer-template]");
 const addDesignerButton = document.getElementById("add-designer-button");
@@ -26,8 +29,16 @@ estimationDurationInput.addEventListener("input", () => {
   }
 });
 
+// // change cost according to the input change of estimation cost
+// estimationCostInput.addEventListener("input", () => {
+//   for (let i = 0; i < estimationCostFields.length; i++) {
+//     estimationCostFields[i].textContent = estimationCostInput.value;
+//   }
+// });
+
 // add designer
 function addDesigner() {
+  console.log("hello");
   const addDesignerClone = addDesignerTemplate.content.cloneNode(true);
   addDesignerContainer.appendChild(addDesignerClone);
 
@@ -36,9 +47,22 @@ function addDesigner() {
 
   for (let i = 0; i < designerCountField.length; i++) {
     designerCountField[i].textContent = designerCount;
+    designerCountField[i].value = designerCount;
+  }
+
+  const designerTagFields = addDesignerContainer.getElementsByClassName("tag");
+  const designerCategoryFields = addDesignerContainer.getElementsByClassName("category");
+  const numberOfDesignersFields = addDesignerContainer.getElementsByClassName("number-of-designers");
+  const estimationCostFields = addDesignerContainer.getElementsByClassName("designer-cost");
+  for (let i = 0; i < designerCountField; i++) {
+    designerTagFields[i].setAttribute('name', 'designer-tag-' + i);
+    designerCategoryFields[i].setAttribute('name', 'designer-category-' + i);
+    numberOfDesignersFields[i].setAttribute('name', 'number-of-designers-' + i);
+    estimationCostFields[i].setAttribute('name', 'vendor-cost-' + i);
   }
 }
-
+// unique[0].setAttribute('name', 'hello');
+// const unique = document.getElementsByClassName('unique');
 // add vendor
 function addVendor() {
   const addVendorClone = addVendorTemplate.content.cloneNode(true);
@@ -49,9 +73,33 @@ function addVendor() {
 
   for (let i = 0; i < vendorCountField.length; i++) {
     vendorCountField[i].textContent = vendorCount;
+    vendorCountField[i].value = vendorCount;
+  }
+
+  const vendorTagFields = addVendorContainer.getElementsByClassName("tag");
+  const vendorServiceFields = addVendorContainer.getElementsByClassName("service");
+  const numberOfVendorsFields = addVendorContainer.getElementsByClassName("number-of-vendors");
+  const estimationCostFields = addVendorContainer.getElementsByClassName("vendor-cost");
+  for (let i = 0; i < designerCountField; i++) {
+    vendorTagFields[i].setAttribute('name', 'vendor-tag-' + i);
+    vendorServiceFields[i].setAttribute('name', 'vendor-category-' + i);
+    numberOfVendorsFields[i].setAttribute('name', 'number-of-vendors-' + i);
+    estimationCostFields[i].setAttribute('name', 'vendor-cost-' + i);
   }
 }
 
 addDesignerButton.addEventListener("click", addDesigner);
 
 addVendorButton.addEventListener("click", addVendor);
+
+
+form.addEventListener('submit', function (event) {
+
+	if (event.submitter.matches('#add-designer-button')) {
+		event.preventDefault();
+	}
+  if (event.submitter.matches('#add-vendor-button')) {
+		event.preventDefault();
+	}
+
+});
